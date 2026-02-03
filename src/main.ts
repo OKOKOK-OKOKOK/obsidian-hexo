1030d9db78c5a1af546e8c9182367631d3616594
YAHAHA <2427071055@qq.com>
2026-02-03 12:48:56 +0800

    to be simple

--- src/main.ts
+++ src/main.ts
@@ -71,10 +71,101 @@ export default class HexoSyncPlugin extends Plugin {
   }
 
   /**
-   * 只同步刚刚保存的那个 md 文件
+   * 纯处理路径
+   * @param file
+   * @private
+   */
+  private resolvePaths(file: TFile): ResolvedPaths | null  {/**
+   * 同步开始
+   */
+  this.logger.log(`[INFO] Sync start: ${file.path}`);
+
+    const adapter = this.app.vault.adapter;
+
+    if (!(adapter instanceof FileSystemAdapter)) {
+      new Notice('Hexo Sync only supports local vaults');
+      return null;
+    }
+    /**
+     * ob数据库的位置
+     */
+    const vaultBasePath = adapter.getBasePath();
+
+    /**
+     *  ob中md文件的绝对路径
+     */
+    const absoluteSrcPath = path.join(vaultBasePath, file.path);
+
+    // 只同步 Obsidian Blog 目录
+    if (!absoluteSrcPath.startsWith(this.OBSIDIAN_BLOG_DIR)) {
+      this.logger.log(
+          `[INFO] Skip non-blog file: ${absoluteSrcPath}`
+      );
+      return null;
+    }
+
+    /**
+     * 文件名字（无扩展名）
+     */
+    const fileNameWithoutExt = path.basename(file.name, '.md');
+
+    /**
+     * 需要创建的同名目标文件夹
+     */
+    const targetDir = path.join(
+        this.HEXO_POST_DIR,
+        fileNameWithoutExt
+    );
+
+    /**
+     * md需要去的文件路径
+     */
+    const targetFilePath = path.join(
+        this.HEXO_POST_DIR,
+        file.name
+    );
+    return {
+      absoluteSrcPath,//ob md文件绝对路径
+      targetDir,//需要创建的同名文件夹绝对路径
+      targetFilePath//md文件最后要去的绝对路径
+    }
+  }
+
+  /**
+   * fm处理
+   * @param file
+   * @param rawContent
+   * @private
+   */
+  private processFrontMatter(
+      file: TFile,
+      rawContent: string
+  ): { content: string; changed: boolean }{
+    /**
+     * 解析并规范化Front Matter
+     */
+    const result =this.frontMatter.ensureAndNormalize(file, rawContent);
+
+    /**
+     * 将fm是否改变 写入日志
+     */
+    this.logger.log(
+        result.changed
+            ? `[INFO] Front Matter updated: ${file.name}`
+            : `[INFO] Front Matter unchanged: ${file.name}`
+    );
+    return result;
+
+  }
+  /**
+   * 同步 md 文件
    */
   private syncSingleMarkdown(file: TFile) {
+
     try {
+      /**
+       * 同步开始
+       */
       this.logger.log(`[INFO] Sync start: ${file.path}`);
 
       const adapter = this.app.vault.adapter;
@@ -84,70 +175,27 @@ export default class HexoSyncPlugin extends Plugin {
         return;
       }
 
+//===========================查找数据库，文件路径
+
+//===============================fm流程
       /**
-       * ob数据库的位置
+       * 读取md文件内容
+       * 几个函数都需要这个数据，所以不单独放在一个函数里面
        */
-      const vaultBasePath = adapter.getBasePath();
+      const rawContent = fs.readFileSync(absoluteSrcPath, 'utf-8');
 
-      /**
-       *  ob文件的绝对路径
-       */
-      const absoluteSrcPath = path.join(vaultBasePath, file.path);
-
-      // 只同步 Obsidian Blog 目录
-      if (!absoluteSrcPath.startsWith(this.OBSIDIAN_BLOG_DIR)) {
-        this.logger.log(
-          `[INFO] Skip non-blog file: ${absoluteSrcPath}`
-        );
-        return;
-      }
-
-      /**
-       * 文件名（无扩展名）
-       */
-      const fileNameWithoutExt = path.basename(file.name, '.md');
-
-      //创建同名文件夹
-      const targetDir = path.join(
-        this.HEXO_POST_DIR,
-        fileNameWithoutExt
-      );
-
-      //md文件的目标地址，这个目标地址不能是文件夹，只能是文件
-      const targetFilePath = path.join(
-        this.HEXO_POST_DIR,
-        file.name
-      );
+//==============================创建同名文件夹
 
       if (!fs.existsSync(targetDir)) {
         fs.mkdirSync(targetDir, { recursive: true });
         this.logger.log(
-          `[INFO] Created post directory: ${targetDir}`
+            `[INFO] Created post directory: ${targetDir}`
         );
       }
 
       new Notice(`tips:${this.HEXO_POST_DIR}`);
 
-      /**
-       * 读取md文件内容
-       */
-      const rawContent = fs.readFileSync(absoluteSrcPath, 'utf-8');
-
-      /**
-       * 解析并规范化Front Matter
-       */
-      const result =
-        this.frontMatter.ensureAndNormalize(file, rawContent);
-
-      /**
-       * 将fm是否改变 写入日志
-       */
-      this.logger.log(
-          result.changed
-              ? `[INFO] Front Matter updated: ${file.name}`
-              : `[INFO] Front Matter unchanged: ${file.name}`
-      );
-
+//=================================复制md文件
       /**
        * 写入md文件内容
        */
@@ -170,7 +218,6 @@ export default class HexoSyncPlugin extends Plugin {
       );
       new Notice('Hexo sync failed');
     }
-
   }
 
 }
